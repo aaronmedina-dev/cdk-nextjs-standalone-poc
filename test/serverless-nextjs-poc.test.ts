5621d58c74cc8b87a1f6c09a549c7af2e0cec08f
Aaron Medina <aaron.medina@aligent.com.au>
2024-12-03 23:28:40 +1030

    Initial commit

---
+++ test/serverless-nextjs-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessNextjsPoc from '../lib/serverless-nextjs-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-nextjs-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessNextjsPoc.ServerlessNextjsPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
